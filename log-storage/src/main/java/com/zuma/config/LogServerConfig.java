03cb147e303bbe1f32c0e6c93a081b23e93fe340
ZhengXing <970389745@qq.com>
2017-11-03 09:25:04 +0800

    test

--- log-storage/src/main/java/com/zuma/config/LogServerConfig.java
+++ log-storage/src/main/java/com/zuma/config/LogServerConfig.java
@@ -14,15 +14,49 @@ import java.lang.reflect.Field;
 @Component
 @ConfigurationProperties(prefix = "logServerConfig")
 public class LogServerConfig {
-    public  Integer PORT = 8080;//端口号
-    public  Integer SO_BACKLOG = 128;//TCP第一二握手队列大小
-    public  Integer SO_SNDBUF = 32768;//发送缓冲区大小
-    public  Integer SO_RCVBBUF = 32768;//接收缓冲区大小
-    public  Integer RETRY_NUM = 5;//自动重连次数，超过后停止n秒再次重连
-    public  Integer STOP_TIME = 5000;//重连x次失败后，暂停重连秒数
-    public  Integer READ_IDLE_TIMEOUT = 20;//读取空闲超时时间,秒
-    public  Integer THREAD_NUM = 10;//服务端线程数
-    public  Integer MAX_THREAD_NUM = 20;//服务端最大线程数（线程池全满后，在拒绝策略中扩充）
+    public static Integer PORT = 8080;//端口号
+    public static Integer SO_BACKLOG = 128;//TCP第一二握手队列大小
+    public static Integer SO_SNDBUF = 32768;//发送缓冲区大小
+    public static Integer SO_RCVBBUF = 32768;//接收缓冲区大小
+    public static Integer RETRY_NUM = 5;//自动重连次数，超过后停止n秒再次重连
+    public static Integer STOP_TIME = 5000;//重连x次失败后，暂停重连秒数
+    public static Integer READ_IDLE_TIMEOUT = 20;//读取空闲超时时间,秒
+    public static Integer THREAD_NUM = 10;//服务端线程数
+    public static Integer MAX_THREAD_NUM = 20;//服务端最大线程数（线程池全满后，在拒绝策略中扩充）
 
+    public static void setPORT(Integer PORT) {
+        LogServerConfig.PORT = PORT;
+    }
 
+    public static void setSoBacklog(Integer soBacklog) {
+        SO_BACKLOG = soBacklog;
+    }
+
+    public static void setSoSndbuf(Integer soSndbuf) {
+        SO_SNDBUF = soSndbuf;
+    }
+
+    public static void setSoRcvbbuf(Integer soRcvbbuf) {
+        SO_RCVBBUF = soRcvbbuf;
+    }
+
+    public static void setRetryNum(Integer retryNum) {
+        RETRY_NUM = retryNum;
+    }
+
+    public static void setStopTime(Integer stopTime) {
+        STOP_TIME = stopTime;
+    }
+
+    public static void setReadIdleTimeout(Integer readIdleTimeout) {
+        READ_IDLE_TIMEOUT = readIdleTimeout;
+    }
+
+    public static void setThreadNum(Integer threadNum) {
+        THREAD_NUM = threadNum;
+    }
+
+    public static void setMaxThreadNum(Integer maxThreadNum) {
+        MAX_THREAD_NUM = maxThreadNum;
+    }
 }
